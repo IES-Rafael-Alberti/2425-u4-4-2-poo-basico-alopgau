003d79d616a5758198c3eb4ea54013d18dd398a6
alopgau <alopezg@iesrafaelalberti.es>
2026-01-16 09:39:46 +0100

    Libro ejemplo constructor 2

--- src/main/kotlin/org/iesra/LibroMain.kt
+++ src/main/kotlin/org/iesra/LibroMain.kt
@@ -3,7 +3,9 @@ import Libro
 fun main() {
     val libro1 = Libro("El Quijote","Cervantes", 450, false)
     val libro2 = Libro("Caperucita", "Anónimo", 90, true)
+    val libro3 = Libro("hola", "adios")
     println(libro1)
     println(libro2)
+    println(libro3)
 
 }
